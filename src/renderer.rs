9a4b9f193cfbda9741efd1a9bb7a368db456e765
Tomasz Rudnicki <tomaszrudnicki95@gmail.com>
2026-02-18 22:53:13 +0100

    Add box drawing to renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,5 +1,6 @@
 use log::trace;
 use std::cell::RefCell;
+use std::io::Write;
 use std::rc::Rc;
 
 use crate::config::Config;
@@ -13,31 +14,41 @@ impl Renderer {
         Renderer { config }
     }
 
-    pub fn render_main_menu(&self) {
+    pub fn render_main_menu(&self, user_input: Option<&str>) -> anyhow::Result<()> {
         // TODO consider using crossterm to clear terminal and manipulate its content (for compatibility reasons)
         // TODO let's find size of the terminal and render UI nicely at the top centered
         // TODO Let's add some colors to the menu (something CyberPunk-themed)
         self.clear_screen();
         self.render_logo();
         self.render_main_menu_options();
+        self.render_input_box(user_input, "Enter your choice...")?;
 
         trace!("Main menu rendered");
+        Ok(())
     }
 
-    pub fn render_settings_menu(&self) {
+    pub fn render_settings_menu(&self, user_input: Option<&str>) -> anyhow::Result<()> {
         self.clear_screen();
         self.render_logo();
         self.render_settings_options();
+        self.render_input_box(user_input, "Enter your choice...")?;
 
         trace!("Settings menu rendered");
+        Ok(())
     }
 
-    pub fn render_guessing_screen(&self, original: &str) {
+    pub fn render_guessing_screen(
+        &self,
+        original: &str,
+        user_input: Option<&str>,
+    ) -> anyhow::Result<()> {
         self.clear_screen();
         self.render_logo();
         self.render_original_phrase(original);
+        self.render_input_box(user_input, "Enter your answer...")?;
 
         trace!("Game screen rendered for phrase: {}", original);
+        Ok(())
     }
 
     fn clear_screen(&self) {
@@ -47,46 +58,74 @@ impl Renderer {
 
     fn render_logo(&self) {
         println!();
-        println!("  ██████╗ ██╗  ██╗██████╗  █████╗ ███████╗███████╗██╗   ██╗");
-        println!("  ██╔══██╗██║  ██║██╔══██╗██╔══██╗██╔════╝██╔════╝╚██╗ ██╔╝");
-        println!("  ██████╔╝███████║██████╔╝███████║███████╗█████╗   ╚████╔╝ ");
-        println!("  ██╔═══╝ ██╔══██║██╔══██╗██╔══██║╚════██║██╔══╝    ╚██╔╝  ");
-        println!("  ██║     ██║  ██║██║  ██║██║  ██║███████║███████╗   ██║   ");
-        println!("  ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ");
+        println!("   ██████╗ ██╗  ██╗██████╗  █████╗ ███████╗███████╗██╗   ██╗   ");
+        println!("   ██╔══██╗██║  ██║██╔══██╗██╔══██╗██╔════╝██╔════╝╚██╗ ██╔╝   ");
+        println!("   ██████╔╝███████║██████╔╝███████║███████╗█████╗   ╚████╔╝    ");
+        println!("   ██╔═══╝ ██╔══██║██╔══██╗██╔══██║╚════██║██╔══╝    ╚██╔╝     ");
+        println!("   ██║     ██║  ██║██║  ██║██║  ██║███████║███████╗   ██║      ");
+        println!("   ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝      ");
         println!();
 
         trace!("Logo rendered");
     }
 
     fn render_main_menu_options(&self) {
-        println!("  What do you want to do?\n");
-        println!("   [Enter]  New game");
-        println!("   [S]      Settings");
-        println!("   [Q]      Quit");
+        println!("   What do you want to do?\n");
+        println!("    [Enter]  New game");
+        println!("    [S]      Settings");
+        println!("    [Q]      Quit");
         println!();
 
         trace!("Main menu options rendered");
     }
 
+    fn render_input_box(&self, text: Option<&str>, placeholder_text: &str) -> anyhow::Result<()> {
+        let box_width = self.config.borrow().input_box_width;
+        let text_width = box_width - 2;
+
+        let top_border = format!("┌{}┐", "─".repeat(box_width));
+        let bottom_border = format!("└{}┘", "─".repeat(box_width));
+        let text_lines = if let Some(text) = text {
+            // TODO split text into chunks per line
+            // TODO combine formats in a loop for multiple lines
+            format!("│ {}{} │", text, " ".repeat(text_width - text.len()))
+        } else {
+            format!(
+                "│ \x1b[90m{}\x1b[0m{} │",
+                placeholder_text,
+                " ".repeat(text_width - placeholder_text.len())
+            )
+        };
+
+        println!(" {} ", top_border);
+        println!(" {} ", text_lines);
+        println!(" {} ", bottom_border);
+        print!("\x1b[2A\x1b[2C");
+        std::io::stdout().flush()?;
+
+        trace!("Input box rendered");
+        Ok(())
+    }
+
     fn render_settings_options(&self) {
-        println!("  Settings\n");
+        println!("   Settings\n");
         println!(
-            "   [D] Database URI: {}",
+            "    [D] Database URI: {}",
             self.config.borrow().db_conn_string
         );
         println!(
-            "   [P] Phrases per round: {}",
+            "    [P] Phrases per round: {}",
             self.config.borrow().phrases_per_round
         );
-        println!("   [S] Save");
-        println!("   [Q] Quit");
+        println!("    [S] Save");
+        println!("    [Q] Quit");
         println!();
 
         trace!("Settings options rendered");
     }
 
     fn render_original_phrase(&self, original: &str) {
-        println!("Sentence: {}\n", original);
+        println!("   Sentence: {}\n", original);
         trace!("Original phrase rendered: {}", original);
     }
 }
